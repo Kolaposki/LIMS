d13ef62ba4a09d9eb1edd6262d0719c71c1162e0
Oshodi Kolapo <oshodikolapo@gmail.com>
2021-06-03 16:59:21 +0100

    :hammer: :rocket: Re-implement patient model

--- patient/models.py
+++ patient/models.py
@@ -1,126 +1,78 @@
+from django_resized import ResizedImageField
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from shortuuidfield import ShortUUIDField
 
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
 
-class BaseQuerySet(models.QuerySet):
-    def delete(self):
-        self.update(archived=timezone.now())
+def patient_image_directory_path(instance, filename):
+    return f'patients/{instance.type}_{instance.collected_on}'
 
 
-class BaseModelManager(models.Manager):
-    def get_queryset(self):
-        return BaseQuerySet(self.model, using=self._db).filter(archived__isnull=True)
+GENDER = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+)
+
+BLOODGROUP = (
+    ("A+", "A+"),
+    ("B+", "B+"),
+    ("A-", "A-"),
+    ("B-", "B-"),
+    ("O-", "O-"),
+    ("O+", "O+"),
+)
+
+GENOTYPE = (
+    ("AA", "AA"),
+    ("AS", "AS"),
+    ("SS", "SS"),
+    ("SC", "SC"),
+)
 
 
-# Create your models here.
-class BaseModel(models.Model):
-    archived = models.DateTimeField(blank=True, null=True, editable=False)
-    last_modified = models.DateTimeField(auto_now=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-
-    objects = BaseModelManager()
-
-    def archive(self, using=None, keep_parents=False):
-        self.archived = timezone.now()
-        super(BaseModel, self).save(using=using)
-
-    def delete(self, using=None, keep_parents=False):
-        self.archive(using, keep_parents)
-
-    def get_admin_url(self):
-        return reverse("admin:%s_%s_change" % (self._meta.app_label, self._meta.model_name), args=(self.id,))
-
-    def __str__(self):
-        try:
-            if self.title:
-                return self.title
-        except AttributeError:
-            pass
-        try:
-            if self.name:
-                return self.name
-        except AttributeError:
-            pass
-        return ""
-
-    class Meta:
-        abstract = True
-        ordering = ['-last_modified']
-
-
-# Create your models here.
-
-class Gender(models.TextChoices):
-    MALE = 'M', _('Male')
-    FEMALE = 'F', _('Female')
-    NON_BINARY = 'N', _('Non Binary')
-    OTHERS = 'O', _('I Prefer not to Say')
-
-
-class BloodGroup(models.TextChoices):
-    A_POSITIVE = 'A1', _('A+')
-    B_POSITIVE = 'B1', _('B+')
-    O_POSITIVE = 'O1', _('A+')
-    A_NEGATIVE = 'A0', _('A-')
-    B_NEGATIVE = 'B0', _('B-')
-    O_NEGATIVE = 'O0', _('O-')
-
-
-class Genotype(models.TextChoices):
-    AA = 'AA', _('AA')
-    AS = 'AS', _('AS')
-    SS = 'SS', _('SS')
-    SC = 'SC', _('SC')
-
-
-class Patient(BaseModel):
+class Patient(models.Model):
     """
         Patient should be gotten through api. Should be added through admin portal
     """
 
     manager = models.ForeignKey(User, on_delete=models.CASCADE)
-    patient_avi = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None, blank=True,
-                                    null=True)
-    patient_no = models.CharField(max_length=50, blank=True, null=True)
+    patient_avi = ResizedImageField(size=[500, 300], quality=100, upload_to=patient_image_directory_path, null=True,
+                                    blank=True)
+    patient_no = models.CharField(max_length=50, blank=True, null=True)  # todo : to be removed
 
-    first_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
-    middle_name = models.CharField(max_length=50, blank=True, null=True)
-    dob = models.DateField(("Date of Birth"), auto_now=False, auto_now_add=False)
+    first_name = models.CharField(max_length=50, blank=True, null=True)  # todo : to be removed
+    last_name = models.CharField(max_length=50, blank=True, null=True)  # todo : to be removed
+    middle_name = models.CharField(max_length=50, blank=True, null=True)  # todo : to be removed
+    dob = models.DateField(auto_now=False, auto_now_add=False)
     age = models.CharField(max_length=3, blank=True, null=True)
-    gender = models.CharField(
-        max_length=1,
-        choices=Gender.choices,
-        default=Gender.OTHERS,
-        blank=True, null=True
-    )
+    gender = models.CharField(choices=GENDER, max_length=50, null=True, blank=True)
     mothers_maiden_name = models.CharField(max_length=50, blank=True, null=True)
     occupation = models.CharField(max_length=50, blank=True, null=True)
 
     home_address = models.CharField(max_length=100, blank=True, null=True)
     phone_number = models.CharField(max_length=12, blank=True, null=True)
     alt_phone_number = models.CharField(max_length=12, blank=True, null=True)
-    email = models.EmailField(_(""), max_length=254, blank=True, null=True)
 
-    blood_group = models.CharField(
-        max_length=2,
-        choices=BloodGroup.choices,
-        blank=True, null=True
-    )
-    genotype = models.CharField(
-        max_length=2,
-        choices=Genotype.choices,
-        blank=True, null=True
-    )
+    blood_group = models.CharField(choices=BLOODGROUP, max_length=2, null=True, blank=True)
+    genotype = models.CharField(choices=GENOTYPE, max_length=2, null=True, blank=True)
+
     weight = models.CharField(max_length=50, blank=True, null=True)
     height = models.CharField(max_length=50, blank=True, null=True)
     allergies = models.CharField(max_length=50, blank=True, null=True)
     disable = models.CharField(max_length=50, blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
 
-    other_notes = models.TextField(blank=True, null=True)
+    other_notes = models.TextField(blank=True, null=True)  # todo : to be removed
+    created_at = models.DateTimeField(null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
+    uuid = ShortUUIDField(max_length=5, editable=False, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Patient {self.manager.first_name} {self.manager.last_name}"
